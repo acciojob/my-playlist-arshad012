745be092258701995ae7d4e9601e5c80da3598ba
arshad012 <forarshad99@gmail.com>
2024-10-20 11:47:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
-let a = document.createElement('a');
-a.setAttribute('target', '_blank');
-a.innerText = 'a tag by JS';
+// let a = document.createElement('a');
+// a.setAttribute('target', '_blank');
+// a.innerText = 'a tag by JS';
 
-document.body.appendChild(a);
+// document.body.appendChild(a);
